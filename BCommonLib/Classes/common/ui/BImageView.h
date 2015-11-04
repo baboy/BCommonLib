6e3359f73f653e52755d26240a8c8617f7cdccd7
h3g2010 <h3g2010@gmail.com>
2015-11-04 15:52:23 +0800

    fix bug of bimageview..............

--- BCommonLib/Classes/common/ui/BImageView.h
+++ BCommonLib/Classes/common/ui/BImageView.h
@@ -19,9 +19,9 @@ typedef enum {
 @property (nonatomic, assign) SEL action;
 @property (nonatomic, retain) UIButton *button;
 @property (nonatomic, retain) UILabel *titleLabel;
-
+@property (nonatomic, retain) NSString *imageURL;
 @property (nonatomic, strong) id userInfo;
-
+@property (nonatomic, retain) NSObject *object;
 @property (nonatomic, assign) CGFloat padding;
 
 @property (nonatomic, assign) BImageTitleStyle titleStyle;
